cc79e9295b2d7f73922630d2ff6062ec477e2373
Jokbt <93jonassvensson@gmail.com>
2026-06-08 21:29:32 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'bangolf-app-v2';
+
+// UPDATERAT TILL BARA PNG-FILER
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './image_96b70a.png',
+  './image_9718e7.png',
+  './image_971888.png',
+  './image_97155f.png'
+];
+
+// Installerar service workern
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cachar app-filer');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Hämtar från cachen i första hand (gör appen blixtsnabb)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Returnera filen om den finns cachad, annars hämta via nätet
+        return response || fetch(event.request);
+      })
+  );
+});
