5c47b62c543642d092bb9588b1f66a147fe27bb1
Jokbt <93jonassvensson@gmail.com>
2026-06-09 17:54:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,48 @@
-// Uppdaterad till V6 för att tvinga mobilen att hämta ny information!
-const CACHE_NAME = 'bangolf-app-v6';
+const CACHE_NAME = 'bangolf-app-v7';
 
+// Vi cachar bara grundfilerna här för att garantera att installationen lyckas
 const urlsToCache = [
   './',
-  './index.html',
-  './manifest.json',
-  './image_96b70a.png',
-  './image_9718e7.png',
-  './image_971888.png',
-  './image_97155f.png'
+  'index.html',
+  'manifest.json'
 ];
 
 self.addEventListener('install', event => {
+  // Tvingar den nya uppdateringen att ta över direkt
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+self.addEventListener('activate', event => {
+  // MAGIN: Detta raderar alla gamla, trasiga versioner från telefonens minne!
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Rensar gammal cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', event => {
+  // Hämtar från cache först, annars från nätet (och sparar bilden automatiskt när den laddats)
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(fetchRes => {
+        return caches.open(CACHE_NAME).then(cache => {
+          if (event.request.url.startsWith('http')) {
+            cache.put(event.request, fetchRes.clone());
+          }
+          return fetchRes;
+        });
+      });
+    })
   );
 });
