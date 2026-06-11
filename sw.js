21c2cf16f3601e5e70a87b91b31a9abb31328de5
Jokbt <93jonassvensson@gmail.com>
2026-06-11 18:00:37 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,32 @@
-const CACHE_NAME = 'bangolf-pwa-v1';
+const CACHE_NAME = 'bangolf-pwa-v2';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
+  // Rensar gammal cache när en ny version aktiveras
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
   return self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  // Enkel "Network First" - Försöker alltid hämta från nätet så bilderna inte förstörs
+  // Nätverk först, fallback till cache. Spara automatiskt allt nytt i cachen så appen blir nedladdningsbar.
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request)
+      .then(response => {
+        if (event.request.url.startsWith('http')) {
+          const resClone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, resClone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
